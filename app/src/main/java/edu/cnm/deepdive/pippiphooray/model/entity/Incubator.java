e7df372e3f44c7dd846b6ca601804008a7e10660
Michael Sylvester <msylvester@cnm.edu>
2026-03-13 11:37:33 -0600

    Commit by Claude: Add entity-relationship diagram and start Incubator entity
    Added PDF documentation showing the database schema for the Pip Pip Hooray
    application, including incubator, batch, egg, and candling_observation tables.
    Started implementing the Incubator entity class with basic Room annotations.

---
+++ app/src/main/java/edu/cnm/deepdive/pippiphooray/model/entity/Incubator.java
@@ -0,0 +1,14 @@
+package edu.cnm.deepdive.pippiphooray.model.entity;
+
+import androidx.room.Entity;
+import androidx.room.Index;
+
+@Entity(
+    tableName = "incubator",
+    indices = {
+        @Index(value = "")
+    }
+)
+public class Incubator {
+
+}
