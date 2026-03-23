387764d337c5133f778720b734e86781a170bf34
Michael Sylvester <msylvester@cnm.edu>
2026-03-22 20:43:41 -0600

    Change by Claude: Wire basic navigation for PipPipHooray
    Added MainActivity with NavHostFragment, nav_graph.xml with 5 destinations
    and Safe Args actions, 5 Fragment classes with view binding, and supporting
    layouts. BatchListFragment FAB navigates to BatchDetailFragment; placeholder
    click in BatchDetailFragment navigates to EggDetailFragment.
    
    Prompt: In this project (edu.cnm.deepdive.pippiphooray), I want to set up basic navigation using a single MainActivity with a NavHostFragment and five Fragments: BatchListFragment, BatchDetailFragment, EggDetailFragment, IncubatorsFragment, and SettingsFragment.

---
+++ app/src/main/java/edu/cnm/deepdive/pippiphooray/controller/MainActivity.java
@@ -0,0 +1,66 @@
+/*
+ *  Copyright 2026 CNM Ingenuity, Inc.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+package edu.cnm.deepdive.pippiphooray.controller;
+
+import android.os.Bundle;
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+import dagger.hilt.android.AndroidEntryPoint;
+import edu.cnm.deepdive.pippiphooray.R;
+import edu.cnm.deepdive.pippiphooray.databinding.ActivityMainBinding;
+
+@AndroidEntryPoint
+public class MainActivity extends AppCompatActivity {
+
+  private ActivityMainBinding binding;
+  private AppBarConfiguration appBarConfiguration;
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    EdgeToEdge.enable(this);
+    super.onCreate(savedInstanceState);
+    binding = ActivityMainBinding.inflate(getLayoutInflater());
+    setContentView(binding.getRoot());
+    ViewCompat.setOnApplyWindowInsetsListener(binding.navHostFragment, (v, insets) -> {
+      Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+      v.setPadding(0, 0, 0, systemBars.bottom);
+      return insets;
+    });
+    setSupportActionBar(binding.toolbar);
+    NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+    appBarConfiguration = new AppBarConfiguration.Builder(
+        R.id.batch_list_fragment,
+        R.id.incubators_fragment,
+        R.id.settings_fragment
+    ).build();
+    NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+  }
+
+  @Override
+  public boolean onSupportNavigateUp() {
+    NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+    return NavigationUI.navigateUp(navController, appBarConfiguration)
+        || super.onSupportNavigateUp();
+  }
+
+}
