814bf43c6bb4e744b53f525fdd995cd37215fdbe
Michael Sylvester <msylvester@cnm.edu>
2026-03-26 19:21:41 -0600

    Added Hilt module for database.

---
+++ app/src/main/java/edu/cnm/deepdive/pippiphooray/service/database/DatabaseModule.java
@@ -0,0 +1,62 @@
+package edu.cnm.deepdive.pippiphooray.service.database;
+
+import android.content.Context;
+import androidx.room.Room;
+import dagger.Module;
+import dagger.Provides;
+import dagger.hilt.InstallIn;
+import dagger.hilt.android.qualifiers.ApplicationContext;
+import dagger.hilt.components.SingletonComponent;
+import edu.cnm.deepdive.pippiphooray.model.dao.BatchDao;
+import edu.cnm.deepdive.pippiphooray.model.dao.CandlingObservationDao;
+import edu.cnm.deepdive.pippiphooray.model.dao.EggDao;
+import edu.cnm.deepdive.pippiphooray.model.dao.EggGroupDao;
+import edu.cnm.deepdive.pippiphooray.model.dao.IncubatorDao;
+import javax.inject.Singleton;
+
+@Module
+@InstallIn(SingletonComponent.class)
+public class DatabaseModule {
+
+  @Provides
+  @Singleton
+  public PipPipDatabase provideDatabase(@ApplicationContext Context context) {
+    return Room.databaseBuilder(
+            context,
+            PipPipDatabase.class,
+            PipPipDatabase.NAME
+        )
+        .build();
+  }
+
+  @Provides
+  @Singleton
+  public BatchDao provideBatchDao(PipPipDatabase database) {
+    return database.getBatchDao();
+  }
+
+  @Provides
+  @Singleton
+  public CandlingObservationDao provideCandlingObservationDao(PipPipDatabase database) {
+    return database.getCandlingObservationDao();
+  }
+
+  @Provides
+  @Singleton
+  public EggDao provideEggDao(PipPipDatabase database) {
+    return database.getEggDao();
+  }
+
+  @Provides
+  @Singleton
+  public EggGroupDao provideEggGroupDao(PipPipDatabase database) {
+    return database.getEggGroupDao();
+  }
+
+  @Provides
+  @Singleton
+  public IncubatorDao provideIncubatorDao(PipPipDatabase database) {
+    return database.getIncubatorDao();
+  }
+
+}
